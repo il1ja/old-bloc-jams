b7cb80cfc908501e5e6027f0c12aa54a1c7b333d
il1ja <dalibor.ilijevski@gmail.com>
2015-12-27 21:38:34 +0100

    finished checkpoint 23

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
-
+var pointsArray = document.getElementsByClassName('point');
+ 
+var animatePoints = function(points) {
     var revealFirstPoint = function() {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
@@ -25,4 +25,19 @@ var animatePoints = function() {
     revealFirstPoint();
     revealSecondPoint();
     revealThirdPoint();
+};
+
+window.onload = function() {
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;    
+    window.addEventListener('scroll', function(event) {
+        if (document.body.scrollTop >= scrollDistance) {
+            
+            animatePoints(pointsArray);   
+        }
+    });
 };
